aade337faa6e3c79e7ab3f93d65d5da8fb5a02e7
aadonoso <aadonoso@LDN2-76MT462.usersad.everis.int>
2017-07-21 15:02:35 +0100

    configuracion con docker

--- Test-ConfigServer/src/main/java/test/springboot/App.java
+++ Test-ConfigServer/src/main/java/test/springboot/App.java
@@ -3,8 +3,6 @@ package test.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
 @EnableConfigServer
